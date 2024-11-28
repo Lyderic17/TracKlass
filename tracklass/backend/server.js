cc416b2c18ab09af21a42c24214949934e7a69f8
Lyderic Violot <lyderic.violot@viacesi.fr>
2024-11-28 12:20:17 +0100

    allow cors

--- tracklass/backend/server.js
+++ tracklass/backend/server.js
@@ -16,6 +16,8 @@ connectDB();
 const allowedOrigins = [
   'http://localhost:4200', // Local development
   'https://tracklass.vercel.app', // Deployed frontend
+  'https://tracklass-6gsarnu05-lyderics-projects-d9645fc0.vercel.app', // Deployed frontend
+  'https://tracklass-9y38q5ni2-lyderics-projects-d9645fc0.vercel.app/'
 ];
 
 // CORS
@@ -32,6 +34,20 @@ app.use(cors({
   allowedHeaders: ['Content-Type', 'x-auth-token'],
 }));
 
+// Add custom headers for preflight responses
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin || '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization,x-auth-token');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end(); // Respond to preflight requests
+    return;
+  }
+
+  next();
+});
 // Middleware
 app.use(express.json());
 
